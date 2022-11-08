97b736dea4bed4c85840410cfde6ef04cf292a03
diveshshubham <divesh.shubham@gmail.com>
2022-11-08 19:10:32 +0530

    remove duplicats folder

--- middleware/controller/defaultPage/defaultPage.js
+++
@@ -1,6 +0,0 @@
-const homePage = async (req, res) => {
-    res.status(200).send({
-        message: " 👋 Hi, Welcome here "
-    })
-}
-module.exports = { homePage }
--- middleware/controller/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    uploadController: require('./uploadController/uploadConroller'),
-    defaultPage:require('./defaultPage/defaultPage'),
-    userController:require('./userController/userController')
-}
